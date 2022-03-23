d63b020656a4b285316244e159e8eaa4eb463671
elizabeth berube <elizabethdberube@gmail.com>
2022-03-23 11:07:54 -0400

    Added more of the basic structure.

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require('express');
+const routes = require('./routes');
 const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
 
-
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
+// set Handlebars as template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// use routes
+app.use(routes);
 
+// sync models to the database and turns on server
+sequelize.sync({ force: false }).then(() => {
 
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 
-
-
-
-
-
-
-
-app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-    console.log(`App listening at http://localhost:${PORT} 👍`);
 });
+
