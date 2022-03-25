a5580bab15b06829a1b49b3f05cf168fbfbd3e3a
elizabeth berube <elizabethdberube@gmail.com>
2022-03-25 14:44:05 -0400

    Small changes

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -3,8 +3,19 @@ const Blog = require('../models/Blog');
 
 // get blog route
 router.get('/', async (req, res) => {
-    res.render('home', { Blog });
+    res.render('home', { blogData });
 });
 
 module.exports = router;
 
+
+
+// // get all dishes
+// router.get('/', async (req, res) => {
+//     res.render('all', { dishes });
+// });
+
+// // get one dish
+// router.get('/dish/:num', async (req, res) => {
+//     return res.render('dish', dishes[req.params.num - 1]);
+// });
