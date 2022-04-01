004a81e73881fa40e24e14e0f1b03079c3c93a58
elizabeth berube <elizabethdberube@gmail.com>
2022-03-31 20:41:46 -0400

    added Comment Model

---
+++ models/Comment.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+// extends off Sequelize's Model class
+class Comment extends Model { }
+
+// fields and rules for Blog model
+Comment.init(
+
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+
+        comments: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+
+            references: {
+                model: 'user',
+                key: 'id',
+                unique: false
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Comment'
+    }
+);
+
+module.exports = Comment; 
